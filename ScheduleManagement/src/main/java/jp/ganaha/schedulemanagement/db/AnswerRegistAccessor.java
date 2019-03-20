9f91c76b7df8711b953990c7f82d8844bace9069
NEC <NEC@NEC-PC>
2019-03-20 22:40:26 +0900

    20190320

--- ScheduleManagement/src/main/java/jp/ganaha/schedulemanagement/db/AnswerRegistAccessor.java
+++ ScheduleManagement/src/main/java/jp/ganaha/schedulemanagement/db/AnswerRegistAccessor.java
@@ -14,7 +14,17 @@ public class AnswerRegistAccessor {
 	 * イベント情報を取得
 	 */
 	public Map<String, Object> getEventData(String eventUrl) {
-		Map<String, Object> row = jdbcTemplate.queryForMap("select * from TB_MST_EVENT where EVENT_ID = ?", eventUrl);
+		Map<String, Object> row = jdbcTemplate.queryForMap("select * from TB_MST_EVENT where EVENT_URL = ?", eventUrl);
+		return row;
+	}
+
+	/**
+	 * 候補日を取得
+	 * @param eventId
+	 * @return 取得結果
+	 */
+	public Map<String, Object> getEventDate(String eventId) {
+		Map<String, Object> row = jdbcTemplate.queryForMap("select * from TB_MST_EVENT_DATE where EVENT_ID = ?", eventId);
 		return row;
 	}
 
